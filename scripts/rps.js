dfb21aa211c2b03c92653df69c91f3543187b569
Chris Guard <christopher.guard@gmail.com>
2020-10-16 17:27:51 +0100

    Add computerPlay function

--- scripts/rps.js
+++ scripts/rps.js
@@ -4,3 +4,26 @@
 
 const lowerBiasLevel = 1 / 3;
 const upperBiasLevel = 2 / 3;
+
+// Function to produce computer move
+
+function computerPlay(lower, upper) {
+
+    let computerChoice = Math.random();
+
+    switch(true) {
+
+        case (0 <= computerChoice) && (computerChoice < lower):
+            return "rock";
+            break;
+        
+        case (lower <= computerChoice) && (computerChoice < upper):
+            return "paper";
+            break;
+
+        case (upper <= computerChoice) && (computerChoice < 1):
+            return "scissors";
+            break;
+    }
+
+} 
